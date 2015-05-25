e336412f8fcd9337760e20106c3c2a659314f14d
Bryan Harrell <bryan@lifeofbrybry.com>
2015-05-25 22:53:30 +0200

    Initial Character Counter Implementation
    Needs Refactoring

---
+++ Counter.py
@@ -0,0 +1,22 @@
+from docx import Document
+import glob
+
+totalLengthInCharacters = 0
+lengthInCharacters = 0
+count = 0
+documentProgress = Document()
+documentProgress.add_paragraph()
+paragraphsProgress = documentProgress.paragraphs
+
+for files in (glob.glob("../Documents/*.docx")):
+        document = Document(files)
+        paragraphs = document.paragraphs
+        count = 0
+        for paragraph in paragraphs:
+            for run in paragraph.runs:
+                lengthInCharacters = lengthInCharacters + len(run.text.encode('utf8'))
+                documentProgress.add_paragraph(run.text)
+
+totalLengthInCharacters = lengthInCharacters + totalLengthInCharacters;
+print ((totalLengthInCharacters)/5.0/1500)
+documentProgress.save('demo.docx')
